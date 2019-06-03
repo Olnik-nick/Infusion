7397dba6b595b543a3a2c5cc4aec610a3ff04142
Olnik-nick <Oleg-olnik@mail.ru>
2019-06-03 17:52:27 +0300

    2

--- mailn.js
+++ mailn.js
@@ -2,9 +2,5 @@ $(document).ready(function(){
 	$('.our-projects__desk[filter="house"]').click(function{
 		$('.our-projects__photos').hide(300);
 	});
-
-		$('.slider-text').slick({
-
-		});
-
+	
 });
